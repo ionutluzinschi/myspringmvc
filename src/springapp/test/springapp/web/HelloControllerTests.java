2dc412b4f8a8b959396bce87ba372e772d2e503e
ionutluzinschi <iluzinschi@gmail.com>
2017-03-19 15:26:37 +0200

    First commit

---
+++ src/springapp/test/springapp/web/HelloControllerTests.java
@@ -0,0 +1,17 @@
+package springapp.test.springapp.web;
+
+import junit.framework.TestCase;
+import org.springframework.web.servlet.ModelAndView;
+import springapp.web.HelloController;
+
+/**
+ * Created by ionut on 3/19/2017.
+ */
+public class HelloControllerTests extends TestCase {
+
+    public void testHandleRequestView() throws Exception{
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);
+        assertEquals("hello.jsp", modelAndView.getViewName());
+    }
+}
---
+++ src/springapp/web/HelloController.java
@@ -0,0 +1,28 @@
+package springapp.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by ionut on 3/19/2017.
+ */
+public class HelloController implements Controller {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        logger.info("Returning hello view");
+
+        return new ModelAndView("hello.jsp");
+    }
+
+}
